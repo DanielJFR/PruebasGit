f9396520dc5fbfb6376b5083b869d3cdfb03162a
Daniel Fernandez Rey <daniel.fernandezrey@servexternos.isban.es>
2021-08-31 16:30:38 +0200

    Comentario añadido desde github

--- src/es/pruebas/principal/ClasePrincipal.java
+++ src/es/pruebas/principal/ClasePrincipal.java
@@ -1,7 +1,7 @@
 package es.pruebas.principal;
 
 public class ClasePrincipal {
-
+        //Comentario añadido desde github
 	public static void main(String[] args) {
 	     System.out.println("Esta es mi clase principal");
          System.out.println("Mi segundo comentario");
