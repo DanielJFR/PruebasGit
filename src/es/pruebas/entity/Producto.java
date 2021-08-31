036eb2618311ac841c46ffed2f3208a4f1a217b4
Dani-GitHub <daniel.fernandez@servexternos.isban.es>
2021-08-31 17:33:10 +0200

    Subiendo clase producto

---
+++ src/es/pruebas/entity/Producto.java
@@ -0,0 +1,19 @@
+package es.pruebas.entity;
+
+public class Producto {
+ private int precio;
+ private String nombre;
+public int getPrecio() {
+	return precio;
+}
+public void setPrecio(int precio) {
+	this.precio = precio;
+}
+public String getNombre() {
+	return nombre;
+}
+public void setNombre(String nombre) {
+	this.nombre = nombre;
+}
+ 
+}
